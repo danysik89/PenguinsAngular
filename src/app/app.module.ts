b975b7793ea85de7b4768ebfb07c61506d03488c
Danylo Goryachko <danilogoryachko@gmail.com>
2018-03-28 23:34:53 +0300

    starter template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
